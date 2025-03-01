03700c7954d52eeaea80996cfa1e7b0258246917
Ataur-Rahaman <ataurfb4445@gmail.com>
2025-03-02 01:26:58 +0600

    added script for realtime updated date

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
 // Theme color change
-let bgColors = ['#F6F0F0', '#E8F9FF', '#73C7C7', '#C5BAFF', '#E5D9F2'];
+let bgColors = ['#F4F7FF', '#F6F0F0', '#E8F9FF', '#73C7C7', '#C5BAFF', '#E5D9F2'];
 document.getElementById('theme-button').addEventListener('click', function(event){
     const theme = document.getElementById('theme');
     theme.style.backgroundColor = bgColors[Math.floor(Math.random() * bgColors.length)]
 })
+
+
+// realtime date
+function updateDate (){
+    const d = new Date()
+    document.getElementById('date-calander').innerHTML = d.toDateString();
+}
+updateDate();
+setInterval(updateDate, 60000);
