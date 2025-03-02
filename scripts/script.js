92fc939a2a284fc14b0fa48d052048692f920b7d
Ataur-Rahaman <ataurfb4445@gmail.com>
2025-03-03 00:13:16 +0600

    added script for redirect to blog page

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
+// redirect to blog page
+document.getElementById('go').addEventListener('click', function(){
+    window.location.href = 'blog.html';
+});
+
 // Theme color change
 let bgColors = ['#F4F7FF', '#F6F0F0', '#E8F9FF', '#73C7C7', '#C5BAFF', '#E5D9F2'];
 document.getElementById('theme-button').addEventListener('click', function(event){
     const theme = document.getElementById('theme');
     theme.style.backgroundColor = bgColors[Math.floor(Math.random() * bgColors.length)]
 })
-
 
 // realtime date
 function updateDate (){
@@ -53,5 +57,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
-
-
